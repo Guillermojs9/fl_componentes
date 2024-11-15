e1c6b03d500e7d0e19b0dc8acf92099dcad34f8b
Guillermo Robles <grobles64@ieszaidinvergeles.org>
2024-11-15 10:02:44 +0100

    FloatingActionButton

--- fl_componentes/lib/screens/alert_screen.dart
+++ fl_componentes/lib/screens/alert_screen.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 
 class AlertScreen extends StatelessWidget {
-   
   const AlertScreen({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('AlertScreen'),
+    return Scaffold(
+      body: const Center(
+        child: Text('AlertScreen'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.green,
+        child: const Icon(Icons.close),
+        onPressed: () => Navigator.pop(context),
       ),
     );
   }
