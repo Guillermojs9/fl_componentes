3add4506fbc46098b0993af19425c665c4cca9bd
Guillermo Robles <grobles64@ieszaidinvergeles.org>
2024-11-15 09:16:02 +0100

    FadeInImage hasta borderRadius

--- fl_componentes/lib/widgets/custom_card_tipo_2.dart
+++ fl_componentes/lib/widgets/custom_card_tipo_2.dart
@@ -5,13 +5,25 @@ class CustomCardTipo2 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Card(
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      //shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
       child: Column(
         children: [
-          Image(
-            image: NetworkImage('https://astelus.com/wp-content/uploads/2024/04/Lago-Moraine-Parque-Nacional-Banff-Alberta-Canada.jpg')
-            )
-          ],
+          const FadeInImage(
+            image: NetworkImage(
+                'https://astelus.com/wp-content/uploads/2024/04/Lago-Moraine-Parque-Nacional-Banff-Alberta-Canada.jpg'),
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            width: double.infinity,
+            height: 260,
+            fit: BoxFit.cover,
+            fadeInDuration: Duration(milliseconds: 300),
+          ),
+          Container(
+              alignment: AlignmentDirectional.centerEnd,
+              padding: const EdgeInsets.only(top: 10, bottom: 10, right: 10),
+              child: const Text("Norway"))
+        ],
       ),
     );
   }
