162f2a1026da156b126b331f285623d581b6062b
Guillermo <grobles64@ieszaidinvergeles.org>
2024-11-29 09:23:19 +0100

    Estilos inputs

--- fl_componentes/lib/theme/app_theme.dart
+++ fl_componentes/lib/theme/app_theme.dart
@@ -5,27 +5,35 @@ class AppTheme {
   static const Color primary = Colors.green;
 
   static final ThemeData lightTheme = ThemeData.light().copyWith(
-    primaryColor: primary,
-    //AppBar Theme
-    appBarTheme: const AppBarTheme(
-        color: primary,
-        titleTextStyle: TextStyle(color: Colors.white, fontSize: 24),
-        iconTheme: IconThemeData(color: Colors.white)),
-    //ListTile Theme
-    listTileTheme: const ListTileThemeData(iconColor: primary),
-    //TextButton Theme
-    textButtonTheme: TextButtonThemeData(
-        style: TextButton.styleFrom(foregroundColor: primary)),
-    //FloatingActionButton
-    floatingActionButtonTheme: const FloatingActionButtonThemeData(
-      backgroundColor: primary,
-      foregroundColor: Colors.white,
-    ),
-    elevatedButtonTheme: ElevatedButtonThemeData(
-      style: ElevatedButton.styleFrom(
+      primaryColor: primary,
+      //AppBar Theme
+      appBarTheme: const AppBarTheme(
+          color: primary,
+          titleTextStyle: TextStyle(color: Colors.white, fontSize: 24),
+          iconTheme: IconThemeData(color: Colors.white)),
+      //ListTile Theme
+      listTileTheme: const ListTileThemeData(iconColor: primary),
+      //TextButton Theme
+      textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(foregroundColor: primary)),
+      //FloatingActionButton
+      floatingActionButtonTheme: const FloatingActionButtonThemeData(
         backgroundColor: primary,
-        foregroundColor: Colors.white
-      )
-    )
-  );
+        foregroundColor: Colors.white,
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: primary, foregroundColor: Colors.white)),
+      //InputDecoration
+      inputDecorationTheme: InputDecorationTheme(
+          floatingLabelStyle: const TextStyle(color: primary),
+          enabledBorder: OutlineInputBorder(
+              borderSide: const BorderSide(color: primary),
+              borderRadius: BorderRadius.circular(5)),
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(5),
+              borderSide: const BorderSide(color: primary)),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(5),
+          )));
 }
